b0fdb6e1a47f70675a7ceca4f6660a3348d701c4
shamil9 <shamil9@gmail.com>
2021-08-28 14:41:58 +0200

    refactor main setup to base class

---
+++ assets/js/base.js
@@ -0,0 +1,82 @@
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import * as dat from "dat.gui"
+
+import vertexShader from "../shaders/vertex.vert"
+import fragmentShader from "../shaders/fragment.frag"
+
+export default class Base {
+  constructor() {
+    this.gui = new dat.GUI()
+    this.canvas = document.querySelector("canvas.webgl")
+    this.scene = new THREE.Scene()
+    this.textureLoader = new THREE.TextureLoader()
+    this.geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
+    this.material = new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+      side: THREE.DoubleSide,
+      uniforms: {
+        uTime: { value: 0 },
+      },
+    })
+    this.mesh = new THREE.Mesh(this.geometry, this.material)
+    this.scene.add(this.mesh)
+    this.sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+    })
+    this.renderer.setSize(this.sizes.width, this.sizes.height)
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    this.clock = new THREE.Clock()
+    this.setCamera()
+    this.setListeners()
+    this.render()
+  }
+
+  setCamera() {
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      this.sizes.width / this.sizes.height,
+      0.1,
+      100
+    )
+    this.camera.position.x = 1
+    this.camera.position.y = 1
+    this.camera.position.z = 2
+    this.scene.add(this.camera)
+
+    // Controls
+    this.controls = new OrbitControls(this.camera, this.canvas)
+    this.controls.enableDamping = true
+  }
+
+  setListeners() {
+    window.addEventListener("resize", () => {
+      // Update sizes
+      this.sizes.width = window.innerWidth
+      this.sizes.height = window.innerHeight
+
+      // Update camera
+      this.camera.aspect = sizes.width / sizes.height
+      this.camera.updateProjectionMatrix()
+
+      // Update renderer
+      this.renderer.setSize(sizes.width, sizes.height)
+      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    })
+  }
+
+  tick() {}
+
+  render() {
+    this.elapsedTime = this.clock.getElapsedTime()
+    this.tick()
+    this.controls.update()
+    this.renderer.render(this.scene, this.camera)
+    window.requestAnimationFrame(this.render.bind(this))
+  }
+}
