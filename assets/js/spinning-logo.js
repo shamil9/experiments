8987b5a0cea3d91390f3220e7335f5a547c07108
shamil9 <shamil9@gmail.com>
2021-09-05 14:05:53 +0200

    chore: init spining logo

---
+++ assets/js/spinning-logo.js
@@ -0,0 +1,29 @@
+import Base from './base'
+import * as THREE from "three"
+
+import logo from "../models/epic-logo.glb"
+
+class Main extends Base {
+	constructor() {
+		super()
+    this.gltfLoader.load(logo, data => {
+      this.logo = data.scene
+      this.logo.traverse(obj => {
+        if (obj.material) {
+          const material = new THREE.MeshBasicMaterial()
+          material.color.set(0xBC994E)
+          obj.material = material
+        }
+      })
+      this.scene.add(this.logo)
+    })
+	}
+
+	tick() {
+    if (this.logo) {
+      this.logo.rotation.y += Math.PI / 2 * 0.01
+    }
+	}
+}
+
+const main = new Main()
