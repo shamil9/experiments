7e5b53973421ff2413dfa9b1cc12b41b5c81d6d5
shamil9 <shamil9@gmail.com>
2022-12-04 22:58:00 +0100

    chore: use src folder & init image transition module

---
+++ src/js/06-image-transition.js
@@ -0,0 +1,37 @@
+import {
+  Mesh,
+  MeshBasicMaterial,
+  PlaneBufferGeometry,
+  RepeatWrapping,
+  Texture,
+  Vector3,
+} from 'three'
+
+import Base from './base'
+import t1 from '@/assets/images/06-image-transition/01.jpg?url'
+
+export default class ImageTransition extends Base {
+  constructor() {
+    super({
+      controls: true,
+      camera: { fov: 75, near: 1, far: 1000, position: new Vector3(0, 0, 650) },
+    })
+
+    this.width = 1920
+    this.height = 1080
+    this.init()
+  }
+
+  async init() {
+    const map = await this.textureLoader.load(t1)
+    const geometry = new PlaneBufferGeometry(1, 1)
+    const material = new MeshBasicMaterial({
+      map,
+    })
+    const mesh = new Mesh(geometry, material)
+    mesh.scale.set(this.width, this.height, 1)
+    this.scene.add(mesh)
+  }
+}
+
+new ImageTransition()
--- src/js/base.js
+++ src/js/base.js
@@ -1,15 +1,28 @@
-import * as THREE from 'three'
+import {
+  Scene,
+  TextureLoader,
+  WebGLRenderer,
+  Clock,
+  PerspectiveCamera,
+  Vector3,
+} from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
 export default class Base {
-  constructor(options = { controls: true }) {
-    this.controlsEnabled = options.controls
+  constructor(
+    settings = {
+      controls: true,
+      camera: { fov: 75, near: 0.1, far: 100, position: new Vector3(0) },
+    }
+  ) {
+    this.controlsEnabled = settings.controls
     this.gui = new dat.GUI()
     this.canvas = document.querySelector('canvas.webgl')
-    this.scene = new THREE.Scene()
+    this.scene = new Scene()
+    this.textureLoader = new TextureLoader()
     this.gltfLoader = new GLTFLoader()
     const dracoLoader = new DRACOLoader()
     dracoLoader.setDecoderPath(
@@ -21,31 +34,28 @@ export default class Base {
       width: window.innerWidth,
       height: window.innerHeight,
     }
-    this.renderer = new THREE.WebGLRenderer({
+    this.renderer = new WebGLRenderer({
       canvas: this.canvas,
     })
     this.renderer.setSize(this.sizes.width, this.sizes.height)
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    this.clock = new THREE.Clock()
-    this.setCamera()
+    this.clock = new Clock()
+    this.setCamera(settings.camera)
     this.setListeners()
     this.render()
   }
 
-  setCamera() {
-    this.camera = new THREE.PerspectiveCamera(
-      75,
+  setCamera(settings) {
+    this.camera = new PerspectiveCamera(
+      settings.fov,
       this.sizes.width / this.sizes.height,
-      0.1,
-      100
+      settings.near,
+      settings.far
     )
-    this.camera.position.x = 1
-    this.camera.position.y = 1
-    this.camera.position.z = 2
+    this.camera.position.copy(settings.position)
     this.scene.add(this.camera)
 
     // Controls
-    console.log(this.controlsEnabled)
     if (this.controlsEnabled) {
       this.controls = new OrbitControls(this.camera, this.canvas)
       this.controls.enableDamping = true
